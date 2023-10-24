caecad9afeade94fbaeafe33ea9fa12ee212e7a3
yawen.xiao <yawen.xiao@lazada.com>
2023-10-24 11:49:53 +0800

    fix: 🚀 public path

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,16 +1,16 @@
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
+  publicPath: '/dumi-demo',
   outputPath: 'docs-dist',
   themeConfig: {
     name: 'dumi-demo',
-
   },
   resolve: {
     atomDirs: [
-      { type: 'component', dir: 'src/components'},
-      { type: 'hook', dir: 'src/hooks'},
-      { type: 'design-patterns', dir: 'src/design-patterns'},
-    ]
-  }
+      { type: 'component', dir: 'src/components' },
+      { type: 'hook', dir: 'src/hooks' },
+      { type: 'design-patterns', dir: 'src/design-patterns' },
+    ],
+  },
 });
